97103bfaa50db7fee631c3efe4b92f15f3e94e18
Victor Josso <victor@josso.fr>
2025-01-23 01:29:52 +0100

    Tiptap base setup

---
+++ src/app/documents/[documentId]/editor.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { EditorContent, useEditor } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+
+export const Editor = () => {
+  const editor = useEditor({
+    extensions: [StarterKit],
+    content: "<p>Hello World! 🌎️</p>",
+  });
+
+  return (
+    <div>
+      <EditorContent editor={editor} />
+    </div>
+  );
+};
